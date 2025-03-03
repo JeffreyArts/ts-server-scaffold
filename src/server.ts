2de0ddb72bb1f27ab52210420f1a01a3ee515941
Jeffrey Arts <arts@glr.nl>
2025-03-03 23:29:17 +0100

    Add CORS headers

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.json());
 app.use('/', mainRoutes);
 app.use('/products/', productRoutes);
